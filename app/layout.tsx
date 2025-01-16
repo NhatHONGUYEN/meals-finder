4bfba0965f227ec8e6a9bf17efd166e6345801d2
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-16 19:02:37 +0100

    🚧 take off screensize

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import QueryProvider from "@/lib/QueryProvider";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { ScreenSize } from "./components/ScreenSize";
+// import { ScreenSize } from "./components/ScreenSize";
 
 const dosis = Dosis({ subsets: ["latin"] });
 const bitter = Bitter({ subsets: ["latin"] });
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-          <ScreenSize />
+          {/* <ScreenSize /> */}
         </QueryProvider>
       </body>
     </html>
