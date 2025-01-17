e0607ffc243cd109eb9be17019442b47d30c592b
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-17 12:03:04 +0100

    ♻️ refacto our list in errorsearch

--- app/components/ErrorSearch.tsx
+++ app/components/ErrorSearch.tsx
@@ -1,22 +1,28 @@
 export default function Error() {
+  const suggestionError = [
+    "Beef",
+    "Chicken",
+    "Lamb",
+    "Pasta",
+    "Pork",
+    "Seafood",
+    "Vegan",
+    "Vegetarian",
+    "Breakfast",
+    "Goat",
+  ];
+
   return (
     <div className="grid place-content-center bg-white px-4">
       <div className="text-center">
-        <p className="text-2xl font-bold tracking-tight  sm:text-4xl">
+        <p className="text-2xl font-bold tracking-tight sm:text-4xl">
           No meals found.
         </p>
         <p className="mt-4 text-gray-500">But you can try :</p>
         <ul className="grid grid-cols-5 gap-5 mt-10 text-gray-500">
-          <li>Beef</li>
-          <li>Chicken</li>
-          <li>Lamb</li>
-          <li>Pasta</li>
-          <li>Pork</li>
-          <li>Seafood</li>
-          <li>Vegan</li>
-          <li>Vegetarian</li>
-          <li>Breakfast</li>
-          <li>Goat</li>
+          {suggestionError.map((suggestion, index) => (
+            <li key={index}>{suggestion}</li>
+          ))}
         </ul>
       </div>
     </div>
