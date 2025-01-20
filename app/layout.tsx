48a020b0276d377971cfb8897b3e46bff40c4043
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-20 19:26:24 +0100

    🔍  Layout : Add MetaData for imagePreview Linkedin

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,19 @@ const bitter = Bitter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Meal Finder",
   description: "Find the best recipes for your next meal.",
+  openGraph: {
+    title: "Meal Finder",
+    description: "Find the best recipes for your next meal.",
+    images: [
+      {
+        url: "/Recipes.jpg",
+        width: 1200,
+        height: 630,
+        alt: "A delicious meal displayed on a plate",
+      },
+    ],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
